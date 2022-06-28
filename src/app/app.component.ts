04e80c9a184ceebc11f416b580d42a7823b8a3ff
Lars Christian Ersly <lce@bankdata.dk>
2022-06-28 15:36:08 +0200

    Add firebase packages and app config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import firebase from "firebase/compat";
+import initializeApp = firebase.initializeApp;
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'BankdataLinks';
+
+  ngOnInit(): void {
+    // Initialize Firebase
+    const app = initializeApp(environment.firebaseConfig);
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig = {
+    apiKey: "AIzaSyChKnrY_7KIJl7liK9VbVjhajmZ5aqjl94",
+    authDomain: "links-test-df846.firebaseapp.com",
+    projectId: "links-test-df846",
+    storageBucket: "links-test-df846.appspot.com",
+    messagingSenderId: "82420744310",
+    appId: "1:82420744310:web:e5ce505338fc14ace49f57"
+  }
 };
 
 /*
