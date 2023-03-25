b18b155866d18d620e54dc16a71cf88d6e751c29
Lars Christian Ersly <lcersly@gmail.com>
2023-03-25 23:41:51 +0100

    styling of avatar

--- src/app/shared/components/avatar/user-avatar.component.ts
+++ src/app/shared/components/avatar/user-avatar.component.ts
@@ -1,5 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {takeUntil} from 'rxjs/operators';
+import {Component, OnDestroy} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatTooltipModule} from '@angular/material/tooltip';
@@ -12,9 +11,9 @@ import {UserPhotoUrlPipe} from '../../../pipes/user-photo-url.pipe';
 import {AuthService} from '../../../services/auth.service';
 
 @Component({
-  selector: "app-user-avatar",
-  templateUrl: "./user-avatar.component.html",
-  styleUrls: ["./user-avatar.component.scss"],
+  selector: 'app-user-avatar',
+  templateUrl: './user-avatar.component.html',
+  styleUrls: ['./user-avatar.component.scss'],
   standalone: true,
   imports: [
     MatButtonModule,
@@ -27,20 +26,14 @@ import {AuthService} from '../../../services/auth.service';
     NgForOf,
     DisplayNameInitialsPipe,
     UserPhotoUrlPipe,
-  ]
+  ],
 })
-export class UserAvatarComponent implements OnInit, OnDestroy {
-  public user: User | null = null;
-
-  user$:Observable<User|null> = this.authService.status$
+export class UserAvatarComponent implements OnDestroy {
+  user$: Observable<User | null> = this.authService.status$
 
   private onDestroy = new Subject<void>();
 
   constructor(private authService: AuthService) {
-  }
-
-  ngOnInit() {
-    this.user$.pipe(takeUntil(this.onDestroy)).subscribe(user => this.user = user);
   }
 
   ngOnDestroy(): void {
