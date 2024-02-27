bff98943584b8eda2577ce89775a2fe1fbdac262
Lars Christian Ersly <lce@bankdata.dk>
2024-02-27 14:35:53 +0100

    Add export table

--- src/app/features/export/components/export-to-file.component.ts
+++ src/app/features/export/components/export-to-file.component.ts
@@ -1,8 +1,13 @@
-import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, inject} from '@angular/core';
 import {MatButton} from '@angular/material/button';
 import {LinkService} from '../../../services/link.service';
 import {saveAs} from 'file-saver';
 import {DatePipe} from '@angular/common';
+import {Link} from '../../../models/link.model';
+
+type LinkWithStringTags = Link & {
+  stringTags: string[]
+}
 
 @Component({
   selector: 'app-export-to-file',
@@ -19,11 +24,26 @@ import {DatePipe} from '@angular/common';
 })
 export class ExportToFileComponent {
 
-  #linkService = inject(LinkService);
+  linkService = inject(LinkService);
   #datePipe = inject(DatePipe);
 
+  links = computed<LinkWithStringTags[]>(() => {
+    return this.linkService.links().map(link => ({...link, stringTags: link.tags.map(tag => tag.key)}))
+  })
+
+  headers = computed(() => {
+    const links = this.links();
+
+    if (links.length === 0) {
+      return [];
+    }
+
+    let keys = Object.keys(links[0]) as (keyof LinkWithStringTags)[];
+    return keys.filter(value => value !== 'searchString' && value !== 'tags');
+  })
+
   exportToJSON() {
-    const data = this.#linkService.links().map(link => {
+    const data = this.linkService.links().map(link => {
       const {tags, searchString, ...rest} = link;
 
       return ({...rest, tags: tags.map(tag=>tag.key)});
